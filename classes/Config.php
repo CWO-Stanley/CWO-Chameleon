86a708aace65c863c99d31b0b2f6d3d832c84d71
DjSxX <46792280+DjSxX@users.noreply.github.com>
2021-04-02 16:22:53 +0200

    Fix nowplaying in radioplayers

--- classes/Config.php
+++ classes/Config.php
@@ -68,7 +68,7 @@ class Config {
         $this->_mysqlPrefixDev = "chameleon_";
 
         $this->_mysqlAnopeHost = "127.0.0.1";
-        $this->_mysqlAnopeUser = "da_admin";
+        $this->_mysqlAnopeUser = "chatters_anope";
         $this->_mysqlAnopePass = "";
         $this->_mysqlAnopeDbName = "anope";
     }
